4c8f39c8260faed44ea5cd14bb62db177f2fe0f6
Falciron <2159742+Falciron@users.noreply.github.com>
2026-06-27 04:31:00 -0700

    Add tests to cover main game play loop
    Increase the robustness of the testing for the most critical component
    of the game. (There may have been some slight liberties taken in the TDD
    process.)

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,6 +8,73 @@ describe Game do
   end
 
   describe '#play' do
+    context 'when a player selects an invalid column, then a valid column' do
+      subject(:corrected_game) { described_class.new(empty_cage, [confused_player, confused_player]) }
+
+      let(:empty_cage) { instance_double(Cage) }
+      let(:confused_player) { instance_double(Player, number: 1, symbol: '#') }
+
+      before do
+        allow(empty_cage).to receive_messages(column_exists?: true, filled_in_column?: false, full?: false,
+                                              has_four_in_a_line_with_top_of_latest_column?: true)
+        allow(empty_cage).to receive(:add_piece_to_column)
+        allow(confused_player).to receive(:select_column).and_return('fish', '0')
+      end
+
+      it 'sends select_column to confused_player twice' do
+        corrected_game.play
+        expect(confused_player).to have_received(:select_column).twice
+      end
+
+      it 'sends column_exists? to empty_cage twice' do
+        corrected_game.play
+        expect(empty_cage).to have_received(:column_exists?).once
+      end
+
+      it 'sends filled_in_column? to empty_cage twice' do
+        corrected_game.play
+        expect(empty_cage).to have_received(:filled_in_column?).once
+      end
+
+      it 'sends full? to empty_cage twice' do
+        corrected_game.play
+        expect(empty_cage).to have_received(:full?).once
+      end
+
+      it 'sends has_four_in_a_line_with_top_of_latest_column? to empty_cage once' do
+        corrected_game.play
+        expect(empty_cage).to have_received(:has_four_in_a_line_with_top_of_latest_column?).once
+      end
+
+      it 'sends add_piece_to_column to empty_cage once' do
+        corrected_game.play
+        expect(empty_cage).to have_received(:add_piece_to_column).at_least(:once)
+      end
+    end
+
+    context "when a player's move doesn't end the game" do
+      subject(:ongoing_game) { described_class.new(empty_cage, [confused_player, other_player]) }
+
+      let(:empty_cage) { instance_double(Cage) }
+      let(:confused_player) { instance_double(Player, number: 1, symbol: '#') }
+      let(:other_player) { instance_double(Player, number: 2, symbol: '@') }
+
+      before do
+        allow(empty_cage).to receive_messages(column_exists?: true, filled_in_column?: false,
+                                              has_four_in_a_line_with_top_of_latest_column?: false)
+        allow(empty_cage).to receive(:add_piece_to_column)
+        allow(empty_cage).to receive(:full?).and_return(false, true)
+        allow(confused_player).to receive(:select_column).and_return('0')
+        allow(other_player).to receive(:select_column).and_return('1')
+      end
+
+      it 'swaps the current player' do
+        expect { ongoing_game.play }.to change {
+          ongoing_game.instance_variable_get(:@current_player)
+        }.to eq(other_player)
+      end
+    end
+
     context 'when cage is filled without a winner' do
       subject(:full_game) { described_class.new(full_cage, [tied_player, tied_player]) }
 
